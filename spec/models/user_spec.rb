cb72c2a2e8b19fb2b375931722d34dcb6f22b59d
Jeremy Kay <jeremy@nobledesktop.com>
2012-04-19 13:38:22 -0400

    Make a basic User model (including secure passwords)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,12 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  password_digest :string(255)
 #
 
 require 'spec_helper'
@@ -25,6 +26,7 @@ describe User do
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password) }
   	it { should respond_to(:password_confirmation) }
+  	it { should respond_to(:authenticate) }
 
 	it { should be_valid }
 
@@ -76,6 +78,27 @@ describe User do
 	describe "when password confirmation is nil" do
 	  before { @user.password_confirmation = nil }
 	  it { should_not be_valid }
+	end
+
+	describe "return value of authenticate method" do
+	  before { @user.save }
+	  let(:found_user) { User.find_by_email(@user.email) }
+
+	  describe "with valid password" do
+	    it { should == found_user.authenticate(@user.password) }
+	  end
+
+	  describe "with invalid password" do
+	    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+	    it { should_not == user_for_invalid_password }
+	    specify { user_for_invalid_password.should be_false }
+	  end
+	end
+
+	describe "with a password that's too short" do
+	  before { @user.password = @user.password_confirmation = "a" * 5 }
+	  it { should be_invalid }
 	end
 
 end

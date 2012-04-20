e84605c678d3bcbefb7be113c587dceaa5f91af6
Jeremy Kay <jeremy@nobledesktop.com>
2012-04-20 17:19:50 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -41,9 +41,10 @@ describe "UserPages" do
 
 	        it { should have_selector('title', text: user.name) }
 	        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-	      end
 
-	      it { should have_link('Sign out') }
+	        it { should have_link('Sign out') }
+	        
+	      end
 	      
 	    end
 

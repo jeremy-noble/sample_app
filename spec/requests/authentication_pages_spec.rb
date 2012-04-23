8625328be1cb5486d47fda3ad6ea64eefa1f2d22
Jeremy Kay <jeremy@nobledesktop.com>
2012-04-23 17:37:50 -0400

    through step 4

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -17,6 +17,11 @@ describe "Authentication" do
       it { should have_selector('title', text: 'Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
 
+      it { should_not have_link('Profile') }
+      it { should_not have_link('Settings') }
+      it { should_not have_link('Sign out') }
+
+
       describe "after visiting another page" do
 		  before { click_link "Home" }
 		  it { should_not have_selector('div.alert.alert-error') }

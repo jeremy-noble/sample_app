9bf0acc1685cbf6ca87a75b1892edcedde257a41
Jeremy Kay <jeremy@nobledesktop.com>
2012-04-26 16:56:00 -0400

    up to 10.2

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
 	
   factory :user do
-
     sequence(:name)  { |n| "Person #{n}" }
     sequence(:email) { |n| "person_#{n}@example.com"}  
     password "foobar"
@@ -10,7 +9,11 @@ FactoryGirl.define do
     factory :admin do
       admin true
     end
+  end
 
+  factory :micropost do
+    content "Lorem ipsum"
+    user
   end
 
 end
